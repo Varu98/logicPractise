3ad7486417a546ca1c3558e99d864148d3c4034d
Vardaan <vardaan.agarwal.98@gmail.com>
2021-09-21 18:28:11 +0530

    add: ques 7

---
+++ functions/7.js
@@ -0,0 +1,8 @@
+/* Given an array, your function should return the length of the array.
+Example:
+Input: arrayLength([1,5,3,7,8]) ––> Output: 5 */
+const readlineSync = require("readline-sync");
+let arr = [];
+
+arr = parseInt(readlineSync.question("enter the numbers"));
+console.log(arr.length);
