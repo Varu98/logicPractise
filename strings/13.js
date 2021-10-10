7d99c26ab257c07562313290ab773c73eca8f440
Vardaan <vardaan.agarwal.98@gmail.com>
2021-10-10 22:45:32 +0530

    add: ques 13

---
+++ strings/13.js
@@ -0,0 +1,12 @@
+//Write a program that removes the time from the given date string "Wed April 15, 7pm". It should return only the date without the time.
+
+function lessTime(str) {
+  str = str.split(" ").map((e) => {
+    if (e.includes("am") || e.includes("pm")) {
+      return (e = e.replace(" "));
+    }
+    return e;
+  });
+  console.log(str);
+}
+lessTime("thursday 5th of october 5:30pm");
